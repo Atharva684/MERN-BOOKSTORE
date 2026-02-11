eb15d96479eb4ba9271404b3d5cd0d2cd57c13e0
Atharva684 <chmcs2026.atharva.chavan@gmail.com>
2026-02-11 10:25:27 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+   //baseURL: 'http://localhost:3000'
+    'https://mern-bookstore-nrcq.onrender.com'
 })
 export default api
